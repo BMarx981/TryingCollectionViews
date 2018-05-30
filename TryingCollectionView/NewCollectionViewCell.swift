8cbe3b85b43f649d7d2bd823103cb5d71f8fda41
Brian Marx <b.marx981@gmail.com>
2018-05-30 11:12:48 -0400

    Initial Commit

---
+++ TryingCollectionView/NewCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  NewCollectionViewCell.swift
+//  TryingCollectionView
+//
+//  Created by Marx, Brian on 5/24/18.
+//  Copyright © 2018 Marx, Brian. All rights reserved.
+//
+
+import UIKit
+
+class NewCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    
+}
